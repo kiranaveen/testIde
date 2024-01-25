779f6e53ffffad2cbd081af1492e4b9c78ea781a
Naveen Kumar M <84959163+kiranaveen@users.noreply.github.com>
2024-01-25 15:23:23 +0530

    Create ExampleMain.java

---
+++ ExampleMain.java
@@ -0,0 +1,16 @@
+public class ExampleMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        
+        // You can add more code here to perform various tasks
+        // For example:
+        int sum = addNumbers(5, 7);
+        System.out.println("Sum of numbers: " + sum);
+    }
+
+    // Example of a method that adds two numbers
+    private static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
